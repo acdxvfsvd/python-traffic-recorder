d29e36a250a984af27f3bababafd32977f208124
acdxvfsvd <cfg32f@gmail.com>
2017-11-16 11:32:44 +0800

    GG

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ A simple traffic recorder for CTF Pwn problems
 apt install python-libpcap
 apt install libpcap-dev
 apt install python-dpkt
+apt install python-daemon
 pip install pypcap
+```
+## Usage
+```shell
+[sudo] python recorder.py network port filename
+Example:
+sudo python recorder.py eth0 23333 /var/log/pwn.log
 ```
--- recorder.py
+++ recorder.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
 import pcap
 import dpkt
 import argparse
+import daemon
+import time
 
 def capt_data(network, port, filename):
 	pc = pcap.pcap(network)
@@ -10,8 +13,9 @@ def capt_data(network, port, filename):
 		if (p.data.__class__.__name__ == 'IP'):
 			if (p.data.data.data != "" and p.data.data.data != "\n"):
 				src_ip = '%d.%d.%d.%d' % tuple(map(ord, list(p.data.src)))
-				dst_ip = '%d.%d.%d.%d' % tuple(map(ord, list(p.data.src)))
-				d = src_ip + ' -> ' + dst_ip + ' ' + p.data.data.data + '\n'
+				dst_ip = '%d.%d.%d.%d' % tuple(map(ord, list(p.data.dst)))
+				t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ptime))
+				d = t + ' ' + src_ip + ' -> ' + dst_ip + ' ' + p.data.data.data + '\n'
 				with open(filename, 'a') as f:
 					f.write(d)
 					f.write(p.data.data.data.encode("hex") + '\n')
@@ -23,4 +27,5 @@ if __name__ == '__main__':
 	parser.add_argument("port", help = " The port you want to record the traffic")
 	parser.add_argument("filename", help = " The filename you want to write the log")
 	args = parser.parse_args()
-	capt_data(args.network, args.port, args.filename)
+	with daemon.DaemonContext():
+		capt_data(args.network, args.port, args.filename)
