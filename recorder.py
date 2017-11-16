7321453e94ead9f70235983a05b49ab6f8017c81
acdxvfsvd <cfg32f@gmail.com>
2017-11-16 10:20:07 +0800

    GG4

--- recorder.py
+++ recorder.py
@@ -2,7 +2,7 @@ import pcap
 import dpkt
 import argparse
 
-def capt_data(network, port):
+def capt_data(network, port, filename):
 	pc = pcap.pcap(network)
 	pc.setfilter('port ' + port)
 	for ptime, pdata in pc:
@@ -10,7 +10,7 @@ def capt_data(network, port):
 		if (p.data.__class__.__name__ == 'IP'):
 			ip = '%d.%d.%d.%d' % tuple(map(ord, list(p.data.dst)))
 			if (p.data.data.data != ""):
-				print ip, "send %d bytes" % p.data.len
+				print ip, "send %d bytes" % p.data.data.len
 				print p.data.data.data.encode("hex")
 				print p.data.data.data
 
@@ -21,4 +21,4 @@ if __name__ == '__main__':
 	parser.add_argument("port", help = " The port you want to record the traffic")
 	parser.add_argument("filename", help = " The filename you want to write the log")
 	args = parser.parse_args()
-	capt_data(args.network, args.port)
+	capt_data(args.network, args.port, args.filename)
